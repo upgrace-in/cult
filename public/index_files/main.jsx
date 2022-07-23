e3cebbcb62cc2915634c6428da218b80d260b3b4
upgrace-in <thedesiretree47@gmail.com>
2022-07-23 18:59:08 +0530

    work

---
+++ public/index_files/main.jsx
@@ -0,0 +1,19 @@
+var _jsxFileName = "/home/prince/Desktop/billionaire/src/main.jsx";
+import __vite__cjsImport0_react from "/node_modules/.vite/deps/react.js?v=ca44e7af"; const React = __vite__cjsImport0_react.__esModule ? __vite__cjsImport0_react.default : __vite__cjsImport0_react;
+import __vite__cjsImport1_reactDom from "/node_modules/.vite/deps/react-dom.js?v=ca44e7af"; const ReactDOM = __vite__cjsImport1_reactDom.__esModule ? __vite__cjsImport1_reactDom.default : __vite__cjsImport1_reactDom;
+import "/src/index.css";
+import App from "/src/App.jsx";
+import __vite__cjsImport4_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=ca44e7af"; const _jsxDEV = __vite__cjsImport4_react_jsxDevRuntime["jsxDEV"];
+ReactDOM.render(/* @__PURE__ */ _jsxDEV(React.StrictMode, {
+  children: /* @__PURE__ */ _jsxDEV(App, {}, void 0, false, {
+    fileName: _jsxFileName,
+    lineNumber: 8,
+    columnNumber: 5
+  }, this)
+}, void 0, false, {
+  fileName: _jsxFileName,
+  lineNumber: 7,
+  columnNumber: 3
+}, this), document.getElementById("root"));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IjtBQUFBO0FBQ0E7QUFDQTtBQUNBOztBQUVBQSxTQUFTQyxPQUNQLHdCQUFDLE1BQU0sWUFBUDtBQUFBLFlBQ0Usd0JBQUMsS0FBRDtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBREY7QUFBQTtBQUFBO0FBQUE7QUFBQSxVQUdBQyxTQUFTQyxlQUFlLE1BQXhCLENBSkYiLCJuYW1lcyI6WyJSZWFjdERPTSIsInJlbmRlciIsImRvY3VtZW50IiwiZ2V0RWxlbWVudEJ5SWQiXSwic291cmNlcyI6WyIvaG9tZS9wcmluY2UvRGVza3RvcC9iaWxsaW9uYWlyZS9zcmMvbWFpbi5qc3giXSwiZmlsZSI6Ii9ob21lL3ByaW5jZS9EZXNrdG9wL2JpbGxpb25haXJlL3NyYy9tYWluLmpzeCIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBSZWFjdCBmcm9tICdyZWFjdCdcclxuaW1wb3J0IFJlYWN0RE9NIGZyb20gJ3JlYWN0LWRvbSdcclxuaW1wb3J0ICcuL2luZGV4LmNzcydcclxuaW1wb3J0IEFwcCBmcm9tICcuL0FwcCdcclxuXHJcblJlYWN0RE9NLnJlbmRlcihcclxuICA8UmVhY3QuU3RyaWN0TW9kZT5cclxuICAgIDxBcHAgLz5cclxuICA8L1JlYWN0LlN0cmljdE1vZGU+LFxyXG4gIGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdyb290JylcclxuKVxyXG4iXX0=
--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import $ from 'jquery'
+import { useEffect } from 'react';
+
+const { MerkleTree } = require('merkletreejs');
+const keccak256 = require('keccak256');
+window.Buffer = window.Buffer || require("buffer").Buffer
 
 function App() {
+
+  // Merkle tree integration
+  let proof
+  let whitelistAddresses = [
+    "0x50A023476C1619979c67725bE45Be8629ab27Ff0",
+    "0x2f8F0588BC81aAfA41cB0CC847bd1FE255E3Ec56"
+  ]
+
+  const leafNodes = whitelistAddresses.map(addr => keccak256(addr));
+  const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
+
+  // Get the RootHash
+  const rootHash = merkleTree.getRoot();
+
+  useEffect(() => {
+    $('#mint_btn').click(() => {
+      const claimingAddress = $('#user_address').text()
+      for (var i = 0; i < whitelistAddresses.length; i++) {
+        if (claimingAddress === whitelistAddresses[i]) {
+          const hexProof = merkleTree.getHexProof(leafNodes[0])
+          proof = hexProof.toString()
+          break
+        } else {
+          proof = null
+        }
+      }
+      if (proof == null) {
+        alert("You are not whitelisted !!!")
+      } else {
+        $('#hash_proof').html(proof)
+      }
+    })
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
